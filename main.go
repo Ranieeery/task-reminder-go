ac3447fb1eae9d755612fd4288febf1bbea8db9f
Ranieeery <raniery2003@hotmail.com>
2025-03-18 11:29:34 -0300

    feat: add MongoDB connection and update dependencies

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"context"
+	"log"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var collection *mongo.Collection
+
+
 func main() {
+	connectDb()
+	
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Hello World"})
@@ -13,4 +24,20 @@ func main() {
 	if err != nil {
 		return
 	}
+}
+
+func connectDb() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Println("Could not connect to database")
+		log.Fatal(err)
+	}
+
+	_, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	collection = client.Database("taskdbs").Collection("taskss")
+	log.Println("Connected to MongoDB")
 }
