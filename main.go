afe97156548657c1842c80146938dd13a8d0ab1e
Ranieeery <raniery2003@hotmail.com>
2025-04-03 23:59:27 -0300

    feat: migrate from Gin to Fiber and add air.toml configuration

--- main.go
+++ main.go
@@ -1,43 +1,12 @@
 package main
 
 import (
-	"context"
+	"github.com/gofiber/fiber/v2"
 	"log"
-	"time"
-
-	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var collection *mongo.Collection
-
-
 func main() {
-	connectDb()
-	
-	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Hello World"})
-	})
-	err := router.Run(":8080")
-	if err != nil {
-		return
-	}
-}
+	app := fiber.New()
 
-func connectDb() {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-
-	if err != nil {
-		log.Println("Could not connect to database")
-		log.Fatal(err)
-	}
-
-	_, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	collection = client.Database("taskdbs").Collection("taskss")
-	log.Println("Connected to MongoDB")
+	log.Fatal(app.Listen(":4000"))
 }
